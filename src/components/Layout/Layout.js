fc69a360170a84a873aeb24e1a9a2652c170a0df
josemar7 <josemar7@gmail.com>
2018-05-11 10:27:52 +0200

    mi commit!

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Aux from '../../hoc/Auxi';
+import Auxi from '../../hoc/Auxi';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Auxi>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxi>
