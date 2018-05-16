efc5773bb7862e02297dfa04e7490ac70ee06ce1
josemar7 <josemar7@gmail.com>
2018-05-16 17:18:50 +0200

    mi commit!

--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Auxi from '../../hoc/Auxi';
 import Burger from '../Burger/Burger';
 import BuildControls from '../Burger/BuildControls/BuildControls';
+import Modal from '../UI/Modal/Modal';
+import OrderSummary from '../Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -79,6 +81,9 @@ class BurgerBuilder extends Component {
         }
         return (
             <Auxi>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler} 
