11e1905cce616c170da54f6be0a3d6b2606dff88
josemar <josemar7@gmail.com>
2018-06-09 18:06:39 +0200

    mi commit!

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,3 @@
-export {addIngredient, removeIngredient, initIngredients} from './burgerBuilder';
+export {addIngredient, removeIngredient, initIngredients, setIngredients, fetchIngredientsFailed} from './burgerBuilder';
 export {purchaseBurger, purchaseInit, fetchOrders} from './order';
-export {auth, logout, setAuthRedirectPath, authCheckState} from './auth';
+export {auth, logout, setAuthRedirectPath, authCheckState, logoutSucceed, authStart, authSuccess, authFail, checkAuthTimeout} from './auth';
